57e2d712a205dc0620cb77dae1e36b1cba948416
Knerav Kodolikar <knerav@gmail.com>
2021-06-23 15:53:17 +0530

    added student resource, model validations, index changes

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,13 @@
 class Course < ApplicationRecord
+  before_save :make_uppercase
+  
   validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 5, maximum: 40 }
   validates :short_name, presence: true, uniqueness: { case_sensitive: false }, length: { is: 5 }
   validates :description, presence: true, uniqueness: { case_sensitive: false }
+
+  def make_uppercase
+    self.short_name.upcase!
+  end
+
+  private :make_uppercase
 end
