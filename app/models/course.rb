82e02d3694bc7f353787ed0c1fef2004354e0699
Knerav Kodolikar <knerav@gmail.com>
2021-06-24 16:40:05 +0530

    added many-to-many association backend

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,13 +1,16 @@
 class Course < ApplicationRecord
   before_save :make_uppercase
   
+  def make_uppercase
+    self.short_name.upcase!
+  end
+  
   validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 5, maximum: 40 }
   validates :short_name, presence: true, uniqueness: { case_sensitive: false }, length: { is: 5 }
   validates :description, presence: true, uniqueness: { case_sensitive: false }
 
-  def make_uppercase
-    self.short_name.upcase!
-  end
+  has_many :student_courses
+  has_many :students, through: :student_courses
 
   private :make_uppercase
 end
